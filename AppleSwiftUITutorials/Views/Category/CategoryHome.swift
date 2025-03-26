a4891d935141c753f50cc085966f383db83c4830
yuximin <whaledestine@163.com>
2025-03-26 10:53:52 +0800

    add - SwiftUI 与 UIKit 交互

--- AppleSwiftUITutorials/Views/Category/CategoryHome.swift
+++ AppleSwiftUITutorials/Views/Category/CategoryHome.swift
@@ -9,15 +9,12 @@ import SwiftUI
 
 struct CategoryHome: View {
     @Environment(ModelData.self) var modelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationSplitView {
             List {
-                modelData.features[0].image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 200)
-                    .clipped()
+                PageView(pages: modelData.features.map({ FeatureCard(landmark: $0) }))
                     .listRowInsets(EdgeInsets())
                 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
@@ -25,7 +22,19 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environment(modelData)
+            }
         } detail: {
             Text("Select a Landmark")
         }
--- AppleSwiftUITutorials/Views/Profiles/ProfileSummary.swift
+++ AppleSwiftUITutorials/Views/Profiles/ProfileSummary.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ProfileSummary: View {
+    @Environment(ModelData.self) var modelData
     var profile: Profile
     
     var body: some View {
@@ -20,6 +21,34 @@ struct ProfileSummary: View {
                 Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
                 Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    HikeView(hike: modelData.hikes[0])
+                }
             }
         }
     }
@@ -27,4 +56,5 @@ struct ProfileSummary: View {
 
 #Preview {
     ProfileSummary(profile: Profile.default)
+        .environment(ModelData())
 }
